34924dd7df727acf88079fba6ec63efbc655f7a4
Alejandro Gonzalez <alexgve7sv@outlook.com>
2024-03-08 23:37:57 +0100

    update repo

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    protected function successResponse($data, $statusCode = Response::HTTP_OK)
+    {
+        return response()->json(['data' => $data], $statusCode);
+    }
+
+    protected function errorResponse($message, $statusCode)
+    {
+        return response()->json(['error' => $message], $statusCode);
+    }
 }
